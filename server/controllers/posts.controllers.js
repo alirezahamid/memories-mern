881c6d85ec81f623c6897db6fde62140a5d74512
Alireza Hamid <alirezahamid20@gmail.com>
2021-06-19 16:34:55 +0430

    implement post req

--- server/controllers/posts.controllers.js
+++ server/controllers/posts.controllers.js
@@ -15,7 +15,7 @@ export const createPost = async (req, res) => {
   const newPost = new PostMessage(post)
   try {
     await newPost.save()
-    res.status(201).json(postMessages)
+    res.status(201).json(newPost)
   } catch (error) {
     res.status(409).json({ message: error.message })
   }
